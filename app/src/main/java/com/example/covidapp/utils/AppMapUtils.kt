24f858cf7afe01d3a4450dac5de87170d0354737
uladzislau <vlad.hilko@gmail.com>
2021-04-22 10:59:08 +0300

    [COVID_8]: ref mainActivity liveData observe
    Signed-off-by: uladzislau <vlad.hilko@gmail.com>

--- app/src/main/java/com/example/covidapp/utils/AppMapUtils.kt
+++ app/src/main/java/com/example/covidapp/utils/AppMapUtils.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.graphics.drawable.toBitmap
 import com.example.covidapp.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -11,13 +13,34 @@ import com.google.android.gms.maps.model.MarkerOptions
 class AppMapUtils {
 
     companion object {
-        fun setMarkerOptions(context: Context, countryName: String, latLng: LatLng): MarkerOptions {
+
+        private const val zoomLvl = 3f
+
+        private fun setMarkerOptions(
+            context: Context,
+            countryName: String,
+            latLng: LatLng
+        ): MarkerOptions {
             val markerResourcePath = R.drawable.ic_map_pin
-            val bitmapVector = AppCompatResources.getDrawable(context, markerResourcePath)?.toBitmap()
+            val bitmapVector =
+                AppCompatResources.getDrawable(context, markerResourcePath)?.toBitmap()
             return MarkerOptions()
                 .position(latLng)
                 .icon(BitmapDescriptorFactory.fromBitmap(bitmapVector))
                 .title(countryName)
+        }
+
+        fun addMarker(
+            context: Context,
+            map: GoogleMap?,
+            lat: String,
+            lon: String,
+            countryName: String
+        ) {
+            val latLng = LatLng(lat.toDouble(), lon.toDouble())
+            val marker = setMarkerOptions(context, countryName, latLng)
+            map?.addMarker(marker)
+            map?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLvl))
         }
     }
 }
